505af4e0e191d8fd4420ccb6ea77d868831b86f1
akhilesh.jain.sadhan <akhilesh.jain.external@sadhan.co.in>
2019-04-28 17:40:33 +0530

    Initial commit

---
+++ build/precache-manifest.3f4b8b2330143dc7b56b2f0a32b1fcf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960caf728acb629911f05dc7c3b09f0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbbe8723641c16f4759b",
+    "url": "/static/css/main.7a82d7f4.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "dbbe8723641c16f4759b",
+    "url": "/static/js/main.68041cc3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
